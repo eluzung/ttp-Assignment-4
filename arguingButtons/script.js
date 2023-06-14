7243a04713d3da355a016a6914d980c527bea751
Hamza Khaliq <101157193+Hikhal@users.noreply.github.com>
2023-06-14 14:41:10 -0400

    Update script.js

--- arguingButtons/script.js
+++ arguingButtons/script.js
@@ -1,3 +1,9 @@
+// The 'btn1' and 'btn2' buttons' onclick events are handled by this code.
+// When 'btn1' is clicked, 'I'm right' is displayed in an element with the id 'text1', 'text2' is cleared of its message, 'btn1' is hidden, and 'btn2' is revealed.
+// When the "btn2" button is hit, the message "No, I'm right!" appears in the //element with the id "text2," the message in the element with the id "text1" is 
+// cleared, "btn2" is hidden, and "btn1" is revealed.
+
+
 document.getElementById('btn1').onclick = function() {
     document.getElementById('text1').innerHTML = "I'm right"
     document.getElementById('text2').innerHTML = ""
